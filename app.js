89ee0fcb55c065456332f136e9175d54a9ca18f2
Joshua Oyewole <joshpp2013@gmail.com>
2020-08-05 20:39:11 +0100

    changes staged

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-//DOM
+let field = document.createElement('h1');
+field.classList.add('todo-field');
+field.textContent='Hello world';
+console.log(field);
+document.querySelector('.container').insertAdjacentHTML("beforeend", field);
 
+//DOM
 let container = document.querySelector('.container');
 let h = document.querySelector('.h');
 let m = document.querySelector('.m');
@@ -7,8 +12,15 @@ let s = document.querySelector('.s');
 let gm = document.querySelector('.gm');
 let greetingMode = document.querySelector('.greeting-mode');
 let name = document.querySelector('.name');
+let addTodo = document.querySelector('.add-btn');
+
 let greet = 'Good Day!';
-/* let getHour = new Date().getHours(); */
+let value = '';
+
+//VALUE VARIABLE
+name.value = localStorage.getItem('name'); 
+/* value = name.value; */
+
 
 //DYNAMIC TIME
 let dynamicTime = (() =>{
@@ -16,7 +28,7 @@ let dynamicTime = (() =>{
         let getHour = new Date().getHours();
         let getMin = new Date().getMinutes();
         let getSec = new Date().getSeconds();
-        console.log(getHour);
+       
         if(getHour >=22){
             greet = 'What a Lovely Night!';
             container.style.background = 'url(img/night.jpg)';
@@ -48,3 +60,38 @@ let dynamicTime = (() =>{
 
 })();
 
+
+//EVENT LISTENERS FOR GETTING NAMES ENTERED
+name.addEventListener('focus', (e) =>{
+    e.target.value = ' ';
+    e.target.style.borderBottom ='.1rem solid rgb(254, 254, 255)'; 
+});
+
+name.addEventListener('blur', (e) =>{
+    e.target.style.borderBottom ='0';
+    if(name.value == '' || name.value == ' '){
+        value = localStorage.getItem('name');
+        name.value = value;
+    }
+    else{
+        value = name.value;
+        localStorage.setItem('name', value);
+    }
+}); 
+
+//EVENT LISTENERS FOR ADD TODO
+addTodo.addEventListener('click', (e) =>{
+   /* const field = document.createElement('input');
+    field.classList.add('todo-field');
+    field.setAttribute('type', 'text')
+    console.log(field);
+     container.insertAdjacentHTML('beforeend', field);  */
+});
+
+let el1 = document.createElement('div');
+let head = document.createElement('h1');
+head.textContent = 'Welcome to Orisfina Computer Institute';
+let para = document.createElement('p');
+para.textContent = 'This is the paragraph of this long letterheaded';
+el1.appendChild(head); el1.appendChild(para);
+container.innerHTML =el1;
